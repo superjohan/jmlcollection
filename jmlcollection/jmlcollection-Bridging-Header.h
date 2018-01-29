861448c40bee0d2c130ce7c3e389099aafb48b2d
Johan Halin <johan@halin.me>
2018-01-29 21:43:31 +0200

    Added GCDWebServer.
    Some demos work better with it.

---
+++ jmlcollection/jmlcollection-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  jmlcollection-Bridging-Header.h
+//  jmlcollection
+//
+//  Created by Johan Halin on 28/01/2018.
+//  Copyright © 2018 Jumalauta. All rights reserved.
+//
+
+#import "GCDWebServer.h"

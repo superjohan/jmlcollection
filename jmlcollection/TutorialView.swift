7506b28ec10c8010d4b738d7de3085a5e8579583
Johan Halin <johan@halin.me>
2018-02-09 21:31:08 +0200

    Improved tutorial.

--- jmlcollection/TutorialView.swift
+++ jmlcollection/TutorialView.swift
@@ -19,7 +19,50 @@
 
 import Foundation
 import UIKit
+import WebKit
 
 class TutorialView: UIView {
+    let webView: WKWebView
+    let button: UIButton
+    let completion: (TutorialView) -> ()
     
+    init(frame: CGRect, completion: @escaping (TutorialView) -> ()) {
+        let webViewConfiguration = WKWebViewConfiguration()
+        webViewConfiguration.mediaTypesRequiringUserActionForPlayback = []
+        webViewConfiguration.allowsInlineMediaPlayback = true
+        self.webView = WKWebView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height), configuration: webViewConfiguration)
+        self.webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.webView.loadFileURL(Bundle.main.bundleURL.appendingPathComponent("tutorial.html"), allowingReadAccessTo: Bundle.main.bundleURL)
+        self.webView.scrollView.isScrollEnabled = false
+        self.webView.backgroundColor = UIColor.clear
+        self.webView.isOpaque = false
+        self.webView.scrollView.backgroundColor = UIColor.clear
+        
+        self.button = UIButton(type: UIButtonType.custom)
+        self.button.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.button.frame = frame
+        
+        self.completion = completion
+        
+        super.init(frame: frame)
+        
+        self.addSubview(self.webView)
+
+        self.button.addTarget(self, action: #selector(buttonTouched), for: UIControlEvents.touchUpInside)
+        self.addSubview(self.button)
+
+        self.backgroundColor = UIColor.clear
+    }
+    
+    override init(frame: CGRect) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func buttonTouched(button: UIButton) {
+        self.completion(self)
+    }
 }

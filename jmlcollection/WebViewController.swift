aa8d97afc8d06c3325726044f7e938a802b47eb8
Johan Halin <johan@halin.me>
2018-02-09 20:51:11 +0200

    Fixed licenses.

--- jmlcollection/WebViewController.swift
+++ jmlcollection/WebViewController.swift
@@ -2,8 +2,19 @@
 //  WebViewController.swift
 //  jmlcollection
 //
-//  Created by Johan Halin on 28/01/2018.
-//  Copyright © 2018 Jumalauta. All rights reserved.
+//  Copyright 2018 Johan Halin.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import UIKit
